3f0ef4aff48ebf1a092f587b3a8bc5e59cd1f824
gauravsonawane <gauravthalner@gmail.com>
2023-02-09 17:56:42 +0530

    comment section 4

--- NZWalksAPI/NZWalksAPI/Repository/RegionRepository.cs
+++ NZWalksAPI/NZWalksAPI/Repository/RegionRepository.cs
@@ -11,9 +11,55 @@ namespace NZWalksAPI.Repository
         {
             this.nZwalksDbContext = nZWalksDbContext;
         }
+
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id=Guid.NewGuid();
+           await nZwalksDbContext.AddAsync(region);
+            await nZwalksDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+           var region = await nZwalksDbContext.Regions.FirstOrDefaultAsync(x=>x.Id==id);
+            if(region == null)
+            {
+                return null;
+            }
+
+            //delete the region
+            nZwalksDbContext.Regions.Remove(region);
+            await nZwalksDbContext.SaveChangesAsync();
+            return region;
+        }
+
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
           return await nZwalksDbContext.Regions.ToListAsync();
+        }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+          return await nZwalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);            
+
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await nZwalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if(existingRegion == null)
+            {
+                return null;
+            }
+            existingRegion.Code=region.Code;
+            existingRegion.Name=region.Name;
+            existingRegion.Area = region.Area;
+            existingRegion.Lat = region.Lat;
+            existingRegion.Long=region.Long;
+            existingRegion.Population = region.Population;
+            await nZwalksDbContext.SaveChangesAsync();
+            return existingRegion;
         }
     }
 }
